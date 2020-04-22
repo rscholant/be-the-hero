378583c9711ed6ce721fc83e6afb72273277a159
Rafael Scholant <rafael.scholant@hotmail.com>
2020-04-22 16:49:16 -0300

    Authentication changes

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,22 +1,30 @@
-import React, {useState} from 'react';
-import {Link, useHistory} from 'react-router-dom';
-import {FiLogIn} from 'react-icons/fi';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiLogIn } from 'react-icons/fi';
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
 
 import api from '../../services/api';
-export default function Logon(){
-  const [id, setId] = useState('');
+export default function Logon() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const history = useHistory();
 
-  async function handleLogin(e){
+  async function handleLogin(e) {
     e.preventDefault();
     try {
-      const response = await api.post('sessions', {id});
-      localStorage.setItem('ongId', id);
-      localStorage.setItem('ongName', response.data.name);
+      const { data } = await api.post('sessions', { email, password });
+      localStorage.setItem(
+        '@bethehero',
+        JSON.stringify({
+          token: data.token,
+          name: data.name,
+          expireAt: data.expireAt,
+        })
+      );
       history.push('/profile');
     } catch (error) {
       alert('Falha no login, tente novamente.');
@@ -25,21 +33,32 @@ export default function Logon(){
   return (
     <div className="logon-container">
       <section className="form">
-        <img src={logoImg} alt="Be The Heroe"/>
-        <form onSubmit={handleLogin} >
+        <img src={logoImg} alt="Be The Heroe" />
+        <form onSubmit={handleLogin}>
           <h1>Faça seu logon</h1>
-          <input 
-            placeholder="Sua ID"
-            value={id}
-            onChange={ e => setId(e.target.value)}/>
-          <button className="button" type="submit">Entrar</button>
+          <input
+            placeholder="Seu e-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Sua senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button className="button" type="submit">
+            Entrar
+          </button>
 
           <Link to="/register" className="back-link">
-            <FiLogIn size={16} color ="#e02041" />
-          Não tenho cadastro</Link>
+            <FiLogIn size={16} color="#e02041" />
+            Não tenho cadastro
+          </Link>
         </form>
       </section>
-      <img src={heroesImg} alt="Heroes"/>
+      <img src={heroesImg} alt="Heroes" />
     </div>
   );
 }
